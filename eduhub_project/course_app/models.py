2f2c47b5ac3a544489408fedb3e7616a5873847a
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2026-03-10 11:56:01 +0100

    "continue with models implementation"

---
+++ eduhub_project/course_app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.db.models.fields import uuid
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+from enumerations.enums import CourseStatus
+from user_app.models import Instructor
+class Course(models.Model):
+    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title=models.CharField(max_length=255)
+    slug=models.SlugField(unique=True)
+    description=models.TextField()
+    status=models.CharField(max_length=20, choices=CourseStatus.choices, default=CourseStatus.DRAFT)
+    price=models.DecimalField(max_digits=10, decimal_places=2, default=0.00,
+    validators=[MinValueValidator(0.00,message="Price must be greater than 0"), MaxValueValidator(2000.00,message="Price must be less than 2000")])
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    #relationship between Course and Instructor (N-1)
+    instructor=models.ForeignKey(Instructor, on_delete=models.SET_NULL,null=True, blank=True)
+    class Meta:
+        db_table = 'courses'
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+        ordering = ['-created_at']
+        indexes = [
+            models.Index(fields=['title', 'slug']),
+        ]
+    #__str__ is the method used to return the string representation of the object
+    #it's used to display the object in the admin interface
+    #it's also used to display the object in the console
+    #it's also used to display the object in the API
+    def __str__(self)->str:
+        return f"{self.title} - {self.instructor.username}"
---
+++ eduhub_project/enumerations/enums.py
@@ -0,0 +1,27 @@
+from django.db import models
+class CourseStatus(models.TextChoices):
+    DRAFT = 'draft', 'Draft'
+    PUBLISHED = 'published'
+    UNPUBLISHED = 'unpublished'
+    ARCHIVED = 'archived'
+    PROGRESS = 'progress'
+    DELETED = 'deleted'
+
+class InstructorGradeLevel(models.TextChoices):
+    ASSISTANT = 'Assistant'
+    LECTURER = 'Lecturer'
+    PROFESSOR = 'Professor'
+    SENIOR_LECTURER = 'Senior Lecturer'
+    HEAD_OF_DEPARTMENT = 'Head of Department'
+    DEAN = 'Dean'
+    PROVOST = 'Provost'
+    CHANCELLOR = 'Chancellor'
+    OTHER = 'Other'
+
+class StudentLevel(models.IntegerChoices):
+    PRIMARY = 1, 'student level 1'
+    SECONDARY = 2, 'student level 2'
+    HIGH_SCHOOL = 3, 'student level 3'
+    COLLEGE = 4, 'student level 4'
+    UNIVERSITY = 5, 'student level 5'
+    OTHER = 6, 'student level 6'
